67fce3b6289ae8b541e648a83347c03680072a85
MoeRayo <moronfolu.motunrayo1@gmail.com>
2022-05-14 12:32:04 +0100

    feat: updating the concert ticket UI with the genrated ticket with cloudinary transformations

---
+++ components/GeneratedConcertTicket.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div>
+  
+    <cld-image ref="ref" :public-id="publicId">
+    
+      <cld-transformation effect="blur:150"/>
+      <cld-transformation effect="brightness_hsb:-30"/>
+      <cld-transformation :overlay="{fontFamily: 'Metal Mania', fontSize: 60, fontWeight: 'bold', text: concertName, testAlign: 'center'}" color="#fff" opacity="90" crop="fit" width="550" effect="shadow"/>
+      <cld-transformation flags="layer_apply"/>
+      <cld-transformation :overlay="{fontFamily: 'Courgette', fontSize: 15, fontWeight: 'bold', text: `Artist: ${artist}`}" color="#fff" effect="shadow"  crop="fit" width="120" />
+      <cld-transformation flags="layer_apply" gravity="south_east" x="150" y="80" />
+      
+    </cld-image>
+    
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    concertName: { 
+      type: String, 
+      required: true 
+    },
+    artist: { 
+      type: String, 
+      required: true
+    },
+    publicId: { 
+      type: String, 
+      required: true
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-washed-blue vh-100 ph3 ph5-ns">
     <header class="tc pv3">
-      <h1 class="tl  tc tl-ns  mt2 mb0 calisto fw5 f2-ns f3 mid-gray ">Custom Summer Concert Ticket Generator</h1>
+      <h1 class="tl  tc tl-ns mt2 mb0 calisto fw5 f2-ns f3 mid-gray ">Custom Summer Concert Ticket Generator</h1>
     </header>
     <section class="flex flex-wrap justify-between">
-      <div class="w-100 w-80-ns tc tl-ns">
+      <div class="w-100 w-50-ns tc tl-ns">
         <h2 class="calisto fw1 f4-ns f3 ">Select an image</h2>
 
         <div v-for="ticket in tickets" :key="ticket.id" class="fl-ns db tc pa1 grow aspect-ratio--4x6 mb3" :class="ticket.id === imageId ? 'ba bw2 b--yellow ' : ''" @click="selectImage(ticket.id, ticket.publicId)">
@@ -17,12 +17,12 @@
         
           <div class="mb3">
             <label class="mb1 ttc f7 db b" for="concert-name">Concert Name</label>
-            <input id="concert-name" v-model="formData.concertName" type="text" name="concert-name" class="db w-100 pv3 ph2 br2 ba b--black-40 f7 bg-washed-blue" required>
+            <input id="concert-name" v-model="formData.concertName" type="text" name="concert-name" class="db w-80-l w-90-m w-100 pv3 ph2 br2 ba b--black-40 f7 bg-washed-blue" required>
           </div>
 
           <div class="mb3">
             <label class="mb1 ttc f7 db b" for="artist">Artist</label>
-            <input id="artist" v-model="formData.artist" type="text" name="artist" class="db w-100 pv3 ph2 br2 ba b--black-40 f7 bg-washed-blue" required>
+            <input id="artist" v-model="formData.artist" type="text" name="artist" class="db w-80-l w-90-m w-100 pv3 ph2 br2 ba b--black-40 f7 bg-washed-blue" required>
           </div>
 
         </div>
@@ -30,7 +30,16 @@
         <button class="f6 link dim br2 ph3 pv2 mb2 dib white bg-navy ba b--navy pointer" @click="handleSubmit">Generate Ticket</button>
         
       </div>
-      
+       <div class="w-100 w-50-ns pa3">
+        <h2 class="calisto fw1 f4-ns f3 underline">Result</h2>
+        <div v-if="showConcertTicket" class="mt-10">
+          <GeneratedConcertTicket
+            :concertName="formData.concertName"
+            :artist="formData.artist"
+            :publicId="formData.publicId"
+          />
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -41,7 +50,7 @@
       return {
         tickets,
         imageId: null,
-        showCard: false,
+        showConcertTicket: false,
         formData: {
           publicId: null,
           error: false,          
@@ -55,13 +64,13 @@
       this.imageId = imageId
       this.formData.publicId = publicId
       this.formData.error = false
-      this.showCard = false
+      this.showConcertTicket = false
     },
     handleSubmit() {
       if (!this.imageId || (this.formData.concertName === ''|| this.formData.artist === '')) {
         this.formData.error = true
       } else {
-        this.showCard = true 
+        this.showConcertTicket = true 
       }
     },
   }
