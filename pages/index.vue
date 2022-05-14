88157e8af3e044882b042235a85315d65c1577ec
MoeRayo <moronfolu.motunrayo1@gmail.com>
2022-05-14 11:14:32 +0100

    feat: updating the concert ticket UI for responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="bg-washed-blue vh-100">
-    <header class="tc pv3 pv3-ns">
-      <h1 class="tl ph5 mt2 mb0 calisto fw5 f2-ns f3 mid-gray ">Custom Summer Concert Ticket Generator</h1>
+  <div class="bg-washed-blue vh-100 ph3 ph5-ns">
+    <header class="tc pv3">
+      <h1 class="tl  tc tl-ns  mt2 mb0 calisto fw5 f2-ns f3 mid-gray ">Custom Summer Concert Ticket Generator</h1>
     </header>
-    <section class="ph5 flex flex-wrap justify-between">
-      <div class="w-100 w-50-ns">
-        <h2 class="baskerville fw1 f4-ns f3">Select an image</h2>
-        
+    <section class="flex flex-wrap justify-between">
+      <div class="w-100 w-80-ns tc tl-ns">
+        <h2 class="calisto fw1 f4-ns f3 ">Select an image</h2>
+
         <div v-for="ticket in tickets" :key="ticket.id" class="fl-ns db tc pa1 grow aspect-ratio--4x6 mb3" :class="ticket.id === imageId ? 'ba bw2 b--yellow ' : ''" @click="selectImage(ticket.id, ticket.publicId)">
-          <cld-image :public-id="ticket.publicId" crop="scale" width="150" height="100"></cld-image>
+          <cld-image :public-id="ticket.publicId" crop="scale" width="130" height="100"></cld-image>
         </div>
 
         <p v-if="formData.error" class="cb red fw3 i f7">Please fill the required fields</p>
         
-        <div class="m4 cb">
+        <div class="mv4 cb">
         
-          <div class="mb3 w-90">
+          <div class="mb3">
             <label class="mb1 ttc f7 db b" for="concert-name">Concert Name</label>
             <input id="concert-name" v-model="formData.concertName" type="text" name="concert-name" class="db w-100 pv3 ph2 br2 ba b--black-40 f7 bg-washed-blue" required>
           </div>
 
-          <div class="mb3 w-90">
+          <div class="mb3">
             <label class="mb1 ttc f7 db b" for="artist">Artist</label>
             <input id="artist" v-model="formData.artist" type="text" name="artist" class="db w-100 pv3 ph2 br2 ba b--black-40 f7 bg-washed-blue" required>
           </div>
